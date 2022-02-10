2223ee8441824a0da8f9cfd8b4ce2d434903f318
sandy094 <geminisey@gmail.com>
2022-02-10 15:50:50 +0800

    修改每日獎勵v1

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('My First Test',()=>{
-    const user = 'testsandy'
-    const pwd = 'testsandy'
+    const user = 'sandyqa'
+    const pwd = 'qasandy'
     it('Frist login',()=>{
         cy.visit('/')
         // 免責說明
@@ -9,18 +9,33 @@ describe('My First Test',()=>{
         cy.get('.footer-menu > :nth-child(4)').contains('會員').click()
 
         cy.url().should('include','/auth?from=https%3A%2F%2Fdemo.champ.land%2Fmember')
-
+        // auth?from=https%3A%2F%2Fchamp.land%2Fmember
         cy.login(user,pwd)
         cy.url().should('include','/member')
     })
     it('dailymession',()=>{
-        cy.getCookie('showWizard')
-          .then((val) =>{
-            if(val){
-              cy.get(':nth-child(1) > .front > img').click()
-            }
-          })
+      cy.get('.daily-gift-content').then(($daily)=>{
+        if($daily!=0){
+          cy.log('1')
+        }else{
+          cy.log('2')
+        }
+      })
         
 
     })
+
+    // it('daily',function(){
+    //   const whenFailed = function(){
+    //     this.skip()
+    //   }
+    //   const whenSucceded = function(){
+    //     cy.get(':nth-child(1) > .front > img').should('be.visible')
+    //   }
+    //   queryFailed(whenFailed, whenSucceded);
+
+      
+    // }
+    // )
+    
 })
