e0be612ffa46078fec3b0825f21a9057cb6279dd
sandy <geminisey@gmail.com>
2022-02-20 16:01:02 +0800

    改成正式區

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -8,8 +8,7 @@ describe('My First Test', () => {
 
     cy.get('.footer-menu > :nth-child(4)').contains('會員').click()
 
-    cy.url().should('include', '/auth?from=https%3A%2F%2Fdemo.champ.land%2Fmember')
-    // auth?from=https%3A%2F%2Fchamp.land%2Fmember
+    cy.url().should('include', '/auth?from=https%3A%2F%2Fchamp.land%2Fmember')
     cy.login(user, pwd)
     cy.url().should('include', '/member')
   })
@@ -17,7 +16,7 @@ describe('My First Test', () => {
   it('dailymission', () => {
     cy.document().then(($document) => {
       const documentResult = $document.querySelectorAll('.daily-gift-content')
-      cy.log(documentResult.length)
+      // cy.log(documentResult.length)
       if (documentResult.length) {
         cy.log('yes')
         cy.get(':nth-child(1) > .front > img').click()
